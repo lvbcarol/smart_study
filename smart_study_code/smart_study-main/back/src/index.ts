641c80abc1b396c02ac5213241fa79a4b77f58af
Carolina <carolinalansoni@gmail.com>
2025-06-27 10:01:50 -0300

    atualizações pacotes bd

--- smart_study_code/smart_study-main/back/src/index.ts
+++ smart_study_code/smart_study-main/back/src/index.ts
@@ -6,13 +6,11 @@ import usuarioRoutes from './routes/usuario.routes';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/usuario', usuarioRoutes);
 
 mongoose.connect('mongodb+srv://24004308:0TheFlash1@cluster0.ukgdnyc.mongodb.net/smart_study?retryWrites=true&w=majority')
   .then(() => console.log('🟢 MongoDB conectado'))
   .catch((err) => console.error('🔴 Erro MongoDB:', err));
-
-// aqui é importante que o segundo argumento seja `usuarioRoutes`
-app.use('/usuario', usuarioRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
